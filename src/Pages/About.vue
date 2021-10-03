dc252befd68ce2e1e34dd00e3ecb2d3867e2a6cf
AlaaMostapha <alaamoustapha21@gmail.com>
2021-10-03 14:20:03 +0200

    add redirects

---
+++ src/Pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>About page</div>
+    <div>Aboooooooout Details</div>
+      <p>{{ $store.state.status }} - {{$store.state.count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped></style>
